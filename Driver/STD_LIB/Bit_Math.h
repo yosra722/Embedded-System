8658fddcda04ec0b97d92d7104dbefae247b026d
yosra722 <yosra.madkour@gmail.com>
2026-07-29 16:31:30 +0300

    Update Bit_Math.h

--- Driver/STD_LIB/Bit_Math.h
+++ Driver/STD_LIB/Bit_Math.h
@@ -5,10 +5,10 @@
 #define CLR_BIT(REG,BIT_NUM)   REG&= ~(1<<BIT_NUM)
 #define TOG_BIT(REG,BIT_NUM)   REG^= (1<<BIT_NUM)
 
-#define Get_Bit(REG,BIT_NUM)   ((REG>>BITNUM)&0x01)
+#define Get_Bit(REG,BIT_NUM)   ((REG>>BIT_NUM)&0x01)
 
-#define SET_PORT(REG)   REG = (0xFF)
-#define CLR_PORT(REG)   REG = (0x00)
+#define SET_PORT(REG)   REG = 0xFF
+#define CLR_PORT(REG)   REG = 0x00
 #define SET_BYTE(REG, VALUE)	REG=VALUE
 
 #endif
