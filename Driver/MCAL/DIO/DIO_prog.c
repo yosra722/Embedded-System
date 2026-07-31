e9e5d8a2c25841966d813b582b4464ea0116e788
yosra722 <yosra.madkour@gmail.com>
2026-08-01 00:33:04 +0300

    Update DIO_prog.c

--- Driver/MCAL/DIO/DIO_prog.c
+++ Driver/MCAL/DIO/DIO_prog.c
@@ -1,31 +1,38 @@
 #include "Std_Types.h"
 #include "Bit_Math.h"
 
+
 #include "DIO_private.h"
 #include "DIO_config.h"
 #include "DIO_int.h"
 
-
-void DIO_voidsetdirection(u8 PortID,u8 PinID,u8 Direction)
+void DIO_voidInit(void)
+{
+	DDRA = DIO_PORTA_DIRECTIONS;
+	DDRB = DIO_PORTB_DIRECTIONS;
+	DDRC = DIO_PORTC_DIRECTIONS;
+	DDRD = DIO_PORTD_DIRECTIONS;
+}
+void DIO_voidSetPinDirection(u8 PortID,u8 PinID,u8 Direction)
 {
     if(PortID <4 && PinID <8){
         if(Direction == OUTPUT){
             switch (PortID)
             {
-                case PORTA:SET_BIT(DDRA,PinID); break;
-                case PORTB:SET_BIT(DDRB,PinID); break;
-                case PORTC:SET_BIT(DDRC,PinID); break;
-                case PORTD:SET_BIT(DDRD,PinID); break;
+                case PORT_A:SET_BIT(DDRA,PinID); break;
+                case PORT_B:SET_BIT(DDRB,PinID); break;
+                case PORT_C:SET_BIT(DDRC,PinID); break;
+                case PORT_D:SET_BIT(DDRD,PinID); break;
 
             }
         }
         else if(Direction == INPUT){
             switch (PortID)
             {
-                case PORTA:CLR_BIT(DDRA,PinID); break;
-                case PORTB:CLR_BIT(DDRB,PinID); break;
-                case PORTC:CLR_BIT(DDRC,PinID); break;
-                case PORTD:CLR_BIT(DDRD,PinID); break;
+                case PORT_A:CLR_BIT(DDRA,PinID); break;
+                case PORT_B:CLR_BIT(DDRB,PinID); break;
+                case PORT_C:CLR_BIT(DDRC,PinID); break;
+                case PORT_D:CLR_BIT(DDRD,PinID); break;
 
             }
         }
@@ -48,20 +55,20 @@ void DIO_voidSetPortDirection(u8 PortID, u8 Direction)
         {
             switch(PortID)
             {
-                case PORTA:SET_PORT(PORTA);break;
-                case PORTB:SET_PORT(PORTB);break;
-                case PORTC:SET_PORT(PORTC);break;
-                case PORTD:SET_PORT(PORTD);break;
+                case PORT_A:SET_PORT(DDRA);break;
+                case PORT_B:SET_PORT(DDRB);break;
+                case PORT_C:SET_PORT(DDRC);break;
+                case PORT_D:SET_PORT(DDRD);break;
             }
         }
         else if(Direction==INPUT)
         {
             switch(PortID)
             {
-                case PORTA:CLR_PORT(PORTA);break;
-                case PORTB:CLR_PORT(PORTB);break;
-                case PORTC:CLR_PORT(PORTC);break;
-                case PORTD:CLR_PORT(PORTD);break;
+                case PORT_A:CLR_PORT(DDRA);break;
+                case PORT_B:CLR_PORT(DDRB);break;
+                case PORT_C:CLR_PORT(DDRC);break;
+                case PORT_D:CLR_PORT(DDRD);break;
             }
         }
     }
@@ -73,27 +80,27 @@ void DIO_voidSetPortDirection(u8 PortID, u8 Direction)
 
 
 /*OUTPUT MODE*/
-void DIO_voidsetPinValue(u8 PortID,u8 PinID,u8 Value)
+void DIO_voidSetPinValue(u8 PortID,u8 PinID,u8 Value)
 {
     if(PortID <4 && PinID <8){
         if(Value==HIGH)
         {
             switch (PortID)
             {
-                case PORTA:SET_BIT(PORTA,PinID); break;
-                case PORTB:SET_BIT(PORTB,PinID); break;
-                case PORTC:SET_BIT(PORTC,PinID); break;
-                case PORTD:SET_BIT(PORTD,PinID); break;
+                case PORT_A:SET_BIT(PORTA,PinID); break;
+                case PORT_B:SET_BIT(PORTB,PinID); break;
+                case PORT_C:SET_BIT(PORTC,PinID); break;
+                case PORT_D:SET_BIT(PORTD,PinID); break;
             }
         }
 
         else if(Value==LOW){
             switch (PortID)
             {
-                case PORTA:CLR_BIT(PORTA,PinID); break;
-                case PORTB:CLR_BIT(PORTB,PinID); break;
-                case PORTC:CLR_BIT(PORTC,PinID); break;
-                case PORTD:CLR_BIT(PORTD,PinID); break;
+                case PORT_A:CLR_BIT(PORTA,PinID); break;
+                case PORT_B:CLR_BIT(PORTB,PinID); break;
+                case PORT_C:CLR_BIT(PORTC,PinID); break;
+                case PORT_D:CLR_BIT(PORTD,PinID); break;
             }
         }
         else {
@@ -109,13 +116,13 @@ void DIO_voidsetPinValue(u8 PortID,u8 PinID,u8 Value)
 /*OUTPUT MODE*/
 void DIO_voidSetPortValue(u8 PortID, u8 Value)
 {
-    if(PortID <4 && PinID <8){
+    if(PortID < 4 ){
         switch(PortID)
         {
-            case PORTA:SET_BYTE(PORTA,Value);break;
-            case PORTB:SET_BYTE(PORTB,Value);break;
-            case PORTC:SET_BYTE(PORTC,Value);break;
-            case PORTD:SET_BYTE(PORTD,Value);break;
+            case PORT_A:SET_BYTE(PORTA,Value);break;
+            case PORT_B:SET_BYTE(PORTB,Value);break;
+            case PORT_C:SET_BYTE(PORTC,Value);break;
+            case PORT_D:SET_BYTE(PORTD,Value);break;
         }
 
     }
@@ -131,12 +138,12 @@ u8 DIO_u8GetPinValue(u8 PortID,u8 PinID)
 {
     if(PortID < 4 && PinID <8)
     {
-        switch (PORTID)
+        switch (PortID)
         {
-            case PORTA:Get_Bit(PINA,PinID);break;
-            case PORTB:Get_Bit(PINB,PinID);break;
-            case PORTC:Get_Bit(PINC,PinID);break;
-            case PORTD:Get_Bit(PIND,PinID);break;
+            case PORT_A:return Get_Bit(PINA,PinID);
+            case PORT_B:return Get_Bit(PINB,PinID);
+            case PORT_C:return Get_Bit(PINC,PinID);
+            case PORT_D:return Get_Bit(PIND,PinID);
         }
     }
     else
@@ -146,20 +153,20 @@ u8 DIO_u8GetPinValue(u8 PortID,u8 PinID)
 }
 
 /*OUTPUT MODE*/
-void DIO_voidTogglePin(u8 PortID, u8 PinID)
+void DIO_voidTogPin(u8 PortID, u8 PinID)
 {
     if(PortID < 4 && PinID < 8 )
     {
         switch(PortID)
         {
-        case PORTA:TOG_BIT(PORTA,PinID);break;
-        case PORTB:TOG_BIT(PORTB,PinID);break;
-        case PORTC:TOG_BIT(PORTC,PinID);break;
-        case PORTD:TOG_BIT(PORTD,PinID);break;
+        case PORT_A:TOG_BIT(PORTA,PinID);break;
+        case PORT_B:TOG_BIT(PORTB,PinID);break;
+        case PORT_C:TOG_BIT(PORTC,PinID);break;
+        case PORT_D:TOG_BIT(PORTD,PinID);break;
         }
     }
     else {
-
+        //no code
     }
 }
 
